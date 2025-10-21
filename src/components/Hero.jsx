ec5dfaaac6fefec90f5cfaafd2ec999304bb4768
Pratik Ranjan <pratikranjan3011@gmail.com>
2025-10-21 23:47:47 +0530

    Remove distracting animated white dots from hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -92,18 +92,6 @@ export const Hero = ({ onNavigate }) => {
           }}
         ></div>
 
-        {[...Array(15)].map((_, i) => (
-          <div
-            key={i}
-            className="absolute w-2 h-2 bg-white/30 rounded-full animate-pulse"
-            style={{
-              top: `${Math.random() * 100}%`,
-              left: `${Math.random() * 100}%`,
-              animationDelay: `${Math.random() * 3}s`,
-              animationDuration: `${2 + Math.random() * 3}s`,
-            }}
-          ></div>
-        ))}
       </div>
 
       <div className="relative z-10 h-full flex items-center">
